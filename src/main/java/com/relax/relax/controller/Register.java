7637ce594869fffdc87af670930813871094888a
chandan kumar <chandankumarbk6@gmail.com>
2021-04-13 04:27:10 +0530

    Initial commit

---
+++ src/main/java/com/relax/relax/controller/Register.java
@@ -0,0 +1,14 @@
+package com.relax.relax.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class Register {
+    @ResponseBody
+    @RequestMapping("/user")
+    public String getUser(){
+        return "Hello user";
+    }
+}
